7527dcb2e565d147f41b0f07b63a73a5f9b03171
Satpal Tanan <tanansatpal@gmail.com>
2020-08-17 02:58:46 +0530

    Added auth guards for logged in users to create posts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,7 +5,7 @@ import {
   Get,
   Param, Patch,
   Post,
-  Put,
+  Put, UseGuards,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
@@ -13,6 +13,7 @@ import { UserService } from './user.service';
 import { UserDto, UserPartialDto } from './dto/user.dto';
 import { UserListDto } from './dto/user-list.dto';
 import { RegisterDto } from '@users/dto/register.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('api/users')
 export class UsersController {
@@ -21,34 +22,40 @@ export class UsersController {
   }
 
   @Get()
+  @UseGuards(AuthGuard('jwt'))
   findAll(): Promise<UserListDto> {
     return this.userService.findAll();
   }
 
   @Get(':id')
+  @UseGuards(AuthGuard('jwt'))
   async findOne(@Param('id') id: string): Promise<UserDto> {
     return await this.userService.findOne(id);
   }
 
   @Post()
+  @UseGuards(AuthGuard('jwt'))
   @UsePipes(new ValidationPipe())
   async create(@Body() body: RegisterDto): Promise<UserDto> {
     return await this.userService.createUser(body);
   }
 
   @Put(':id')
+  @UseGuards(AuthGuard('jwt'))
   @UsePipes(new ValidationPipe())
   async update(@Param('id') id: string, @Body() userDto: UserDto): Promise<UserDto> {
     return await this.userService.updateUser(id, userDto);
   }
 
   @Patch(':id')
+  @UseGuards(AuthGuard('jwt'))
   @UsePipes(new ValidationPipe())
   async updatePatch(@Param('id') id: string, @Body() userDto: UserPartialDto): Promise<UserDto> {
     return await this.userService.updatePartialUser(id, userDto);
   }
 
   @Delete(':id')
+  @UseGuards(AuthGuard('jwt'))
   async destroy(@Param('id') id: string): Promise<UserDto> {
     return await this.userService.destroyUser(id);
   }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,14 @@ import { UsersController } from './users.controller';
 import { UserService } from './user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from '@users/entity/user.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   controllers: [UsersController],
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [
+    TypeOrmModule.forFeature([UserEntity]),
+    AuthModule,
+  ],
   providers: [UserService],
   exports: [UserService],
 })
