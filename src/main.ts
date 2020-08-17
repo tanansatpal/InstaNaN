2cd9931d1e784ef0b66659689c9f0d569aae1d38
Satpal Tanan <tanansatpal@gmail.com>
2020-08-18 01:18:30 +0530

    Added helmet and enabled CORS

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { runDbMigrations } from '@shared/utils';
+import * as helmet from 'helmet';
 
 const port = process.env.PORT;
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.use(helmet());
+  app.enableCors();
   await runDbMigrations();
   await app.listen(port);
 }
